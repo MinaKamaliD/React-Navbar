3e9c0d1e8c382a43753db5fa9385f1ec16fba853
Mina <minakamali.d@gmail.com>
2023-01-19 13:19:53 +0330

    functional component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar/NavBar";
 
-class App extends Component {
-  render() {
-    return <NavBar></NavBar>;
-  }
-}
+// Class componenet
+
+// class App extends Component {
+//   render() {
+//     return <NavBar></NavBar>;
+//   }
+// }
+
+// Functional component
+
+const App = () => {
+  return <NavBar></NavBar>;
+};
 
 export default App;
